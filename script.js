f5ef9d6c3f9b24c29961b067ceb5e4ff4901a68c
Artur Lipskii <falcon.arthur@gmail.com>
2022-12-04 19:32:17 +0200

    player switching, adding marks works

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
 const gameBoard = {
-    emptyBoard: [1,2,3,4,5,6,7,8],
-    playedBoard: [,,,,,,,,],
-    player1: {
-        name: "Player 1",
-        mark: "X",
-    },
-    player2: {
-        name: "Player 2",
-        mark: "O", 
-    }
+    emptyBoard: [1, 2, 3, 4, 5, 6, 7, 8],
+    playedBoard: [, , , , , , , ,],
+};
+const Player = (name, mark) => {
+    const getName = () => name;
+    const getMark = () => mark;
+    const turn = id => {
+        gameCells[id].innerText = mark;
+        gameBoard.playedBoard[id] = mark;
+    };
+return {getName, getMark, turn};
+};
+const player1 = Player('Player 1', "X");
+const player2 = Player('Player 2', "O");
+let activePlayer = player1;
+const changeActivePlayer = () => {
+    activePlayer === player1 ? activePlayer = player2 : activePlayer = player1;
 }
+
 
 
 const board = document.querySelector('.gameboard');
 board.addEventListener('click', () => {
     if (event.target.classList.contains('cell')) {
         let id = event.target.dataset.id;
-        addMark(id, event.target);
-        // return id;
+        activePlayer.turn(id);
+        changeActivePlayer();
     }
-})
-const drawGameBoard = (function() {
+});
+const drawGameBoard = (function () {
     for (let i = 0; i <= gameBoard.emptyBoard.length; i++) {
         const gameCell = document.createElement('div');
         gameCell.classList.add('cell');
         gameCell.setAttribute('data-id', i);
         board.appendChild(gameCell);
     }
-})()
-const gameCells = document.querySelectorAll('.cell')
+})();
+const gameCells = document.querySelectorAll('.cell');
 
-const addMark = (id,player) => {
-    gameCells[id].innerText = gameBoard.player.mark;
-    console.log(id,event)
-}
+const addMark = (id, player) => {
+    gameCells[id].innerText = gameBoard.player2.mark;
+    console.log(id, event);
+};
