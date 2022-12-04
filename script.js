a312ba9ec58185523ba1b95370bb4e7d8dd0a333
Artur Lipskii <falcon.arthur@gmail.com>
2022-12-05 00:36:05 +0200

    styling

--- script.js
+++ script.js
@@ -1,66 +1,83 @@
-const gameBoard = {
-    emptyBoard: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-    playedBoard: [, , , , , , , ,],
-    winningBoards: [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ],
-};
+//* board arrays and drawing board
+const gameBoard = (() => {
+    let boards = {
+        playedBoard: [,],
+        winningBoards: [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ]
+    };
+    const drawGameBoard = () => {
+        const board = document.querySelector('.gameboard');
+        for (let i = 0; i < 9; i++) {
+            const gameCell = document.createElement('div');
+            gameCell.classList.add('cell');
+            gameCell.setAttribute('data-id', i);
+            board.appendChild(gameCell);
+        }
+        board.addEventListener('click', (e) => {
+            gameController.gameTurn(e);
+        });
+    };
+    drawGameBoard();
+    const gameCells = document.querySelectorAll('.cell');
+    return { boards, drawGameBoard, gameCells };
+})();
+
+
 const Player = (name, mark) => {
     const turn = id => {
-        gameCells[id].innerText = mark;
-        gameCells[id].classList.add('clicked');
-        gameBoard.playedBoard[id] = mark;
-        remainingSpots--;
+        gameBoard.gameCells[id].innerText = mark;
+        gameBoard.gameCells[id].classList.add('clicked');
+        gameBoard.boards.playedBoard[id] = mark;
     };
     return { name, mark, turn };
 };
 const player1 = Player('Player 1', "X");
 const player2 = Player('Player 2', "O");
 
-let activePlayer = player1;
-let winnerDeclared = false;
-let remainingSpots = 9;
 
-const changeActivePlayer = () => {
-    activePlayer === player1 ? activePlayer = player2 : activePlayer = player1;
-};
-
-function checkWinner(player) {
-    gameBoard.winningBoards.forEach((e, i) => {
-        if (gameBoard.playedBoard[e[0]] === activePlayer.mark && gameBoard.playedBoard[e[1]] === player.mark && gameBoard.playedBoard[e[2]] === player.mark) {
-            console.log(`winner is ${player.name}`);
-            winnerDeclared = true;
-        }       
-    })
-    if (remainingSpots === 0 && !winnerDeclared) {
-        console.log("it's a draw");
-        winnerDeclared = true;
+//* game module
+const gameController = (() => {
+    let activePlayer = player1;
+    const gameMode = ["vs", "ai"]; 
+    let winnerDeclared = false;
+    let remainingSpots = 9;
+    const changeActivePlayer = () => {
+        activePlayer === player1 ? activePlayer = player2 : activePlayer = player1;
     };
-}
 
-
-const board = document.querySelector('.gameboard');
-board.addEventListener('click', () => {
-    if (event.target.classList.contains('cell') && !winnerDeclared) {
-        let id = event.target.dataset.id;
-        activePlayer.turn(id);
-        checkWinner(activePlayer);
-        changeActivePlayer();
-    }
-});
-const drawGameBoard = (function () {
-    for (let i = 0; i < gameBoard.emptyBoard.length; i++) {
-        const gameCell = document.createElement('div');
-        gameCell.classList.add('cell');
-        gameCell.setAttribute('data-id', i);
-        board.appendChild(gameCell);
-    }
+    const checkWinner = (player) => {
+        gameBoard.boards.winningBoards.forEach((e) => {
+            if (gameBoard.boards.playedBoard[e[0]] === activePlayer.mark && gameBoard.boards.playedBoard[e[1]] === player.mark && gameBoard.boards.playedBoard[e[2]] === player.mark) {
+                console.log(`winner is ${player.name}`);
+                winnerDeclared = true;
+            }
+        });
+        if (remainingSpots === 0 && !winnerDeclared) {
+            console.log("it's a draw");
+            winnerDeclared = true;
+        };
+    };
+    const gameTurn = (event) => {
+        if (event.target.classList.contains('cell') && !winnerDeclared) {
+            remainingSpots--;
+            let id = event.target.dataset.id;
+            activePlayer.turn(id);
+            checkWinner(activePlayer);
+            changeActivePlayer();
+        }
+    };
+    return { changeActivePlayer, gameTurn };
 })();
-const gameCells = document.querySelectorAll('.cell');
+
+
+
+
+
