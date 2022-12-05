2c9fa446932f1a68fe1c864ad65b8007ddb00403
Artur Lipskii <falcon.arthur@gmail.com>
2022-12-05 10:55:47 +0200

    added navigation and endgame screen

--- script.js
+++ script.js
@@ -46,9 +46,13 @@ const player2 = Player('Player 2', "O");
 //* game module
 const gameController = (() => {
     let activePlayer = player1;
-    const gameMode = ["vs", "ai"]; 
+    const gameMode = ["vs", "ai"];
     let winnerDeclared = false;
     let remainingSpots = 9;
+    const endScreen = document.querySelector('.endgame');
+    const winScreen = document.querySelector('.win');
+    const loseScreen = document.querySelector('.lose');
+    const drawScreen = document.querySelector('.draw');
     const changeActivePlayer = () => {
         activePlayer === player1 ? activePlayer = player2 : activePlayer = player1;
     };
@@ -56,12 +60,15 @@ const gameController = (() => {
     const checkWinner = (player) => {
         gameBoard.boards.winningBoards.forEach((e) => {
             if (gameBoard.boards.playedBoard[e[0]] === player.mark && gameBoard.boards.playedBoard[e[1]] === player.mark && gameBoard.boards.playedBoard[e[2]] === player.mark) {
-                console.log(`winner is ${player.name}`);
+                endScreen.classList.remove('hidden');
+                winScreen.classList.remove('hidden');
+                winScreen.innerText= `winner is ${player.name}`;
                 winnerDeclared = true;
             }
         });
         if (remainingSpots === 0 && !winnerDeclared) {
-            console.log("it's a draw");
+            endScreen.classList.remove('hidden');
+            drawScreen.classList.remove('hidden');
             winnerDeclared = true;
         };
     };
@@ -83,18 +90,57 @@ const interface = (() => {
     const squares = document.querySelector('.gameboard');
     const squaresInfo = document.querySelector('.gameboard-info');
     const settingsscreen = document.querySelector('.settingsscreen');
+    const btnBackToMain = document.querySelector('.backtomain');
     const playGameBtn = document.querySelector('#playgame');
+    const settingsAccept = document.querySelector('.settingsaccept');
+    const settingsCancel = document.querySelector('.settingscancel');
     const settingsBtn = document.querySelector('#settings');
+
     playGameBtn.addEventListener('click', () => {
         mainscreen.classList.toggle('hideleft');
         squares.classList.toggle('showleft');
         squaresInfo.classList.toggle('showleft');
+
     });
     settingsBtn.addEventListener('click', () => {
         mainscreen.classList.toggle('hideright');
         settingsscreen.classList.toggle('showright');
+
     });
-    return {}
+    settingsAccept.addEventListener('click', () => {
+        mainscreen.classList.remove('hideright');
+        mainscreen.classList.add('showleft');
+        settingsscreen.classList.remove('showright');
+        settingsscreen.classList.add('hideleft');
+        defaultClasses();
+
+    });
+    settingsCancel.addEventListener('click', () => {
+        mainscreen.classList.remove('hideright');
+        mainscreen.classList.add('showleft');
+        settingsscreen.classList.remove('showright');
+        settingsscreen.classList.add('hideleft');
+        defaultClasses();
+    });
+    btnBackToMain.addEventListener('click', () => {
+        mainscreen.classList.remove('hideleft');
+        squares.classList.remove('showleft');
+        squaresInfo.classList.remove('showleft');
+        mainscreen.classList.add('showright');
+        squares.classList.add('hideright');
+        squaresInfo.classList.add('hideright');
+        defaultClasses();
+    });
+
+    function defaultClasses() {
+        setTimeout(() => {
+            squares.className = "gameboard";
+            squaresInfo.className = "gameboard-info";
+            mainscreen.className = "mainscreen";
+            settingsscreen.className = "settingsscreen";
+        }, 720);
+    }
+    return {};
 })();
 
 
