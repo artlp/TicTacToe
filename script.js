ac87345e59d094262eba3eae7986c8e5b0127239
Artur Lipskii <falcon.arthur@gmail.com>
2022-12-07 23:42:03 +0200

    flip animations

--- script.js
+++ script.js
@@ -14,14 +14,15 @@ const gameBoard = (() => {
         ]
     };
     const colors = {
-        black: "hsl(0, 80%, 0%)",
-        white: "hsl(0, 100%, 100%)",
-        red: "hsl(1, 77%, 55%)",
-        blue: "hsl(200, 97%, 45%)",
-        yellow: "hsl(65, 100%, 63%)",
-        green: "hsl(88, 100%, 67%)",
-        orange: "hsl(36, 100%, 50%)",
+        // black: "#000000",
+        // white: "#FFFFFF",
+        purple: "#A172E8",
+        blue: "#76D4E8",
+        yellow: "#F2F088",
+        green: "#63F273",
+        orange: "#F2795E",
     };
+
     const markers = {
         cross: "⤫",
         circle: "●",
@@ -52,6 +53,30 @@ const gameBoard = (() => {
 
 //* players module
 const Players = (() => {
+    const pSBC=(p,c0,c1,l)=>{
+        let r,g,b,P,f,t,h,i=parseInt,m=Math.round,a=typeof(c1)=="string";
+        if(typeof(p)!="number"||p<-1||p>1||typeof(c0)!="string"||(c0[0]!='r'&&c0[0]!='#')||(c1&&!a))return null;
+        if(!this.pSBCr)this.pSBCr=(d)=>{
+            let n=d.length,x={};
+            if(n>9){
+                [r,g,b,a]=d=d.split(","),n=d.length;
+                if(n<3||n>4)return null;
+                x.r=i(r[3]=="a"?r.slice(5):r.slice(4)),x.g=i(g),x.b=i(b),x.a=a?parseFloat(a):-1
+            }else{
+                if(n==8||n==6||n<4)return null;
+                if(n<6)d="#"+d[1]+d[1]+d[2]+d[2]+d[3]+d[3]+(n>4?d[4]+d[4]:"");
+                d=i(d.slice(1),16);
+                if(n==9||n==5)x.r=d>>24&255,x.g=d>>16&255,x.b=d>>8&255,x.a=m((d&255)/0.255)/1000;
+                else x.r=d>>16,x.g=d>>8&255,x.b=d&255,x.a=-1
+            }return x};
+        h=c0.length>9,h=a?c1.length>9?true:c1=="c"?!h:false:h,f=this.pSBCr(c0),P=p<0,t=c1&&c1!="c"?this.pSBCr(c1):P?{r:0,g:0,b:0,a:-1}:{r:255,g:255,b:255,a:-1},p=P?p*-1:p,P=1-p;
+        if(!f||!t)return null;
+        if(l)r=m(P*f.r+p*t.r),g=m(P*f.g+p*t.g),b=m(P*f.b+p*t.b);
+        else r=m((P*f.r**2+p*t.r**2)**0.5),g=m((P*f.g**2+p*t.g**2)**0.5),b=m((P*f.b**2+p*t.b**2)**0.5);
+        a=f.a,t=t.a,f=a>=0||t>=0,a=f?a<0?t:t<0?a:a*P+t*p:0;
+        if(h)return"rgb"+(f?"a(":"(")+r+","+g+","+b+(f?","+m(a*1000)/1000:"")+")";
+        else return"#"+(4294967296+r*16777216+g*65536+b*256+(f?m(a*255):0)).toString(16).slice(1,f?undefined:-2)
+    };
 
     // const Player = (name, marker, color) => {
     //     const turn = id => {
@@ -74,10 +99,17 @@ const Players = (() => {
             this.color = color;
         }
         turn(id) {
-            gameBoard.gameCells[id].innerText = this.marker;
-            gameBoard.boards.playedBoard[id] = this.marker;
-            gameBoard.gameCells[id].classList.add('clicked');
-            gameBoard.gameCells[id].style.color = this.color;
+            gameBoard.gameCells[id].classList.add('flip-vertical-right');
+            setTimeout( () => {
+                gameBoard.gameCells[id].innerText = this.marker;
+                gameBoard.boards.playedBoard[id] = this.marker;
+                gameBoard.gameCells[id].classList.add('clicked');
+                // gameBoard.gameCells[id].style.color = this.color;
+                // gameBoard.gameCells[id].style.backgroundColor = pSBC(-0.35,this.color);
+                gameBoard.gameCells[id].style.color = pSBC(-0.55,this.color);
+                gameBoard.gameCells[id].style.backgroundColor = this.color;
+            },200)
+
         }
     }
     const player1 = new Player("Player 1", gameBoard.markersArray[0], gameBoard.colorsArray[0]);
@@ -85,7 +117,6 @@ const Players = (() => {
 
     return { Player, player1, player2 };
 })();
-message.innerText = `${Players.player1.name}, it's your turn`;
 
 //* game module
 const gameController = (() => {
@@ -150,7 +181,7 @@ const Interface = (() => {
         mainscreen.classList.toggle('hideleft');
         squares.classList.toggle('showleft');
         squaresInfo.classList.toggle('showleft');
-
+        message.innerText = `${Players.player1.name}, it's your turn`;
     });
     settingsBtn.addEventListener('click', () => {
         mainscreen.classList.toggle('hideright');
@@ -233,7 +264,7 @@ const Interface = (() => {
         e.addEventListener('click', () => {
             ar.forEach(x => {
                 x.className = "settingscolor";
-            })
+            });
             Players.player1.color = gameBoard.colorsArray[i];
             e.className = "settingscolor";
             selectedOption(e);
@@ -243,7 +274,7 @@ const Interface = (() => {
         e.addEventListener('click', () => {
             ar.forEach(x => {
                 x.className = "settingscolor";
-            })
+            });
             Players.player2.color = gameBoard.colorsArray[i];
             e.className = "settingscolor";
             selectedOption(e);
@@ -253,7 +284,7 @@ const Interface = (() => {
         e.addEventListener('click', () => {
             ar.forEach(x => {
                 x.className = "settingscolor";
-            })
+            });
             Players.player1.marker = gameBoard.markersArray[i];
             e.className = "settingscolor";
             selectedOption(e);
@@ -263,7 +294,7 @@ const Interface = (() => {
         e.addEventListener('click', () => {
             ar.forEach(x => {
                 x.className = "settingscolor";
-            })
+            });
             Players.player2.marker = gameBoard.markersArray[i];
             e.className = "settingscolor";
             selectedOption(e);
