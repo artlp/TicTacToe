0bcf264872506e06f8d59e974250651488329851
Artur Lipskii <falcon.arthur@gmail.com>
2022-12-08 22:23:53 +0200

    endgame screen styling

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //* board arrays and drawing board
 const gameBoard = (() => {
     let boards = {
-        playedBoard: [,],
+        playedBoard: new Array(9),
         winningBoards: [
             [0, 1, 2],
             [3, 4, 5],
@@ -26,7 +26,7 @@ const gameBoard = (() => {
     const markers = {
         cross: "⤫",
         circle: "●",
-        star: "★",
+        star: "✫",
         heart: "🎔",
         note: "♪",
     };
@@ -53,29 +53,30 @@ const gameBoard = (() => {
 
 //* players module
 const Players = (() => {
-    const pSBC=(p,c0,c1,l)=>{
-        let r,g,b,P,f,t,h,i=parseInt,m=Math.round,a=typeof(c1)=="string";
-        if(typeof(p)!="number"||p<-1||p>1||typeof(c0)!="string"||(c0[0]!='r'&&c0[0]!='#')||(c1&&!a))return null;
-        if(!this.pSBCr)this.pSBCr=(d)=>{
-            let n=d.length,x={};
-            if(n>9){
-                [r,g,b,a]=d=d.split(","),n=d.length;
-                if(n<3||n>4)return null;
-                x.r=i(r[3]=="a"?r.slice(5):r.slice(4)),x.g=i(g),x.b=i(b),x.a=a?parseFloat(a):-1
-            }else{
-                if(n==8||n==6||n<4)return null;
-                if(n<6)d="#"+d[1]+d[1]+d[2]+d[2]+d[3]+d[3]+(n>4?d[4]+d[4]:"");
-                d=i(d.slice(1),16);
-                if(n==9||n==5)x.r=d>>24&255,x.g=d>>16&255,x.b=d>>8&255,x.a=m((d&255)/0.255)/1000;
-                else x.r=d>>16,x.g=d>>8&255,x.b=d&255,x.a=-1
-            }return x};
-        h=c0.length>9,h=a?c1.length>9?true:c1=="c"?!h:false:h,f=this.pSBCr(c0),P=p<0,t=c1&&c1!="c"?this.pSBCr(c1):P?{r:0,g:0,b:0,a:-1}:{r:255,g:255,b:255,a:-1},p=P?p*-1:p,P=1-p;
-        if(!f||!t)return null;
-        if(l)r=m(P*f.r+p*t.r),g=m(P*f.g+p*t.g),b=m(P*f.b+p*t.b);
-        else r=m((P*f.r**2+p*t.r**2)**0.5),g=m((P*f.g**2+p*t.g**2)**0.5),b=m((P*f.b**2+p*t.b**2)**0.5);
-        a=f.a,t=t.a,f=a>=0||t>=0,a=f?a<0?t:t<0?a:a*P+t*p:0;
-        if(h)return"rgb"+(f?"a(":"(")+r+","+g+","+b+(f?","+m(a*1000)/1000:"")+")";
-        else return"#"+(4294967296+r*16777216+g*65536+b*256+(f?m(a*255):0)).toString(16).slice(1,f?undefined:-2)
+    const pSBC = (p, c0, c1, l) => {
+        let r, g, b, P, f, t, h, i = parseInt, m = Math.round, a = typeof (c1) == "string";
+        if (typeof (p) != "number" || p < -1 || p > 1 || typeof (c0) != "string" || (c0[0] != 'r' && c0[0] != '#') || (c1 && !a)) return null;
+        if (!this.pSBCr) this.pSBCr = (d) => {
+            let n = d.length, x = {};
+            if (n > 9) {
+                [r, g, b, a] = d = d.split(","), n = d.length;
+                if (n < 3 || n > 4) return null;
+                x.r = i(r[3] == "a" ? r.slice(5) : r.slice(4)), x.g = i(g), x.b = i(b), x.a = a ? parseFloat(a) : -1;
+            } else {
+                if (n == 8 || n == 6 || n < 4) return null;
+                if (n < 6) d = "#" + d[1] + d[1] + d[2] + d[2] + d[3] + d[3] + (n > 4 ? d[4] + d[4] : "");
+                d = i(d.slice(1), 16);
+                if (n == 9 || n == 5) x.r = d >> 24 & 255, x.g = d >> 16 & 255, x.b = d >> 8 & 255, x.a = m((d & 255) / 0.255) / 1000;
+                else x.r = d >> 16, x.g = d >> 8 & 255, x.b = d & 255, x.a = -1;
+            } return x;
+        };
+        h = c0.length > 9, h = a ? c1.length > 9 ? true : c1 == "c" ? !h : false : h, f = this.pSBCr(c0), P = p < 0, t = c1 && c1 != "c" ? this.pSBCr(c1) : P ? { r: 0, g: 0, b: 0, a: -1 } : { r: 255, g: 255, b: 255, a: -1 }, p = P ? p * -1 : p, P = 1 - p;
+        if (!f || !t) return null;
+        if (l) r = m(P * f.r + p * t.r), g = m(P * f.g + p * t.g), b = m(P * f.b + p * t.b);
+        else r = m((P * f.r ** 2 + p * t.r ** 2) ** 0.5), g = m((P * f.g ** 2 + p * t.g ** 2) ** 0.5), b = m((P * f.b ** 2 + p * t.b ** 2) ** 0.5);
+        a = f.a, t = t.a, f = a >= 0 || t >= 0, a = f ? a < 0 ? t : t < 0 ? a : a * P + t * p : 0;
+        if (h) return "rgb" + (f ? "a(" : "(") + r + "," + g + "," + b + (f ? "," + m(a * 1000) / 1000 : "") + ")";
+        else return "#" + (4294967296 + r * 16777216 + g * 65536 + b * 256 + (f ? m(a * 255) : 0)).toString(16).slice(1, f ? undefined : -2);
     };
 
     // const Player = (name, marker, color) => {
@@ -93,27 +94,28 @@ const Players = (() => {
     // const player2 = Player('Player 2', gameBoard.markersArray[1], gameBoard.colorsArray[1]);
 
     class Player {
-        constructor(name, marker, color) {
+        constructor(name, marker, color, score) {
             this.name = name;
             this.marker = marker;
             this.color = color;
+            this.score = score;
         }
         turn(id) {
             gameBoard.gameCells[id].classList.add('flip-vertical-right');
-            setTimeout( () => {
+            gameBoard.boards.playedBoard[id] = this.marker;
+            setTimeout(() => {
                 gameBoard.gameCells[id].innerText = this.marker;
-                gameBoard.boards.playedBoard[id] = this.marker;
                 gameBoard.gameCells[id].classList.add('clicked');
                 // gameBoard.gameCells[id].style.color = this.color;
                 // gameBoard.gameCells[id].style.backgroundColor = pSBC(-0.35,this.color);
-                gameBoard.gameCells[id].style.color = pSBC(-0.55,this.color);
+                gameBoard.gameCells[id].style.color = pSBC(-0.55, this.color);
                 gameBoard.gameCells[id].style.backgroundColor = this.color;
-            },200)
+            }, 200);
 
         }
     }
-    const player1 = new Player("Player 1", gameBoard.markersArray[0], gameBoard.colorsArray[0]);
-    const player2 = new Player("Player 2", gameBoard.markersArray[1], gameBoard.colorsArray[1]);
+    const player1 = new Player("Player 1", gameBoard.markersArray[0], gameBoard.colorsArray[0], 0);
+    const player2 = new Player("Player 2", gameBoard.markersArray[1], gameBoard.colorsArray[1], 0);
 
     return { Player, player1, player2 };
 })();
@@ -128,6 +130,8 @@ const gameController = (() => {
     const winScreen = document.querySelector('.win');
     const loseScreen = document.querySelector('.lose');
     const drawScreen = document.querySelector('.draw');
+    const p1Score = document.querySelector('#p1score');
+    const p2Score = document.querySelector('#p2score');
     const message = document.querySelector('#message');
     const changeActivePlayer = () => {
         activePlayer === Players.player1 ? activePlayer = Players.player2 : activePlayer = Players.player1;
@@ -139,7 +143,13 @@ const gameController = (() => {
             if (gameBoard.boards.playedBoard[e[0]] === player.marker && gameBoard.boards.playedBoard[e[1]] === player.marker && gameBoard.boards.playedBoard[e[2]] === player.marker) {
                 endScreen.classList.remove('hidden');
                 winScreen.classList.remove('hidden');
-                winScreen.innerText = `winner is ${player.name}`;
+                winScreen.style.backgroundColor = player.color;
+                winScreen.style.boxShadow = `0px 0 7px 8px ${player.color}`;
+                winScreen.firstChild.innerText = `The winner is 
+                ${player.name}`;
+                player.score++;
+                p1Score.innerText = `${Players.player1.score}`;
+                p2Score.innerText = `${Players.player2.score}`;
                 winnerDeclared = true;
             }
         });
@@ -158,7 +168,14 @@ const gameController = (() => {
             changeActivePlayer();
         }
     };
-    return { changeActivePlayer, gameTurn, endScreen, winScreen, loseScreen, drawScreen };
+    const nextRound = () => {
+        gameBoard.boards.playedBoard.length = 0;
+        gameBoard.boards.playedBoard.length = 9;
+        gameBoard.drawGameBoard();
+        gameController.remainingSpots = 9;
+    }
+
+    return { changeActivePlayer, gameTurn, endScreen, winScreen, loseScreen, drawScreen, nextRound };
 })();
 
 //*settings and buttons module 
@@ -176,7 +193,6 @@ const Interface = (() => {
     const p1Marker = document.querySelector('#player1marker');
     const p2Marker = document.querySelector('#player2marker');
 
-
     playGameBtn.addEventListener('click', () => {
         mainscreen.classList.toggle('hideleft');
         squares.classList.toggle('showleft');
@@ -189,13 +205,17 @@ const Interface = (() => {
 
     });
     settingsAccept.addEventListener('click', () => {
-        mainscreen.classList.remove('hideright');
-        mainscreen.classList.add('showleft');
-        settingsscreen.classList.remove('showright');
-        settingsscreen.classList.add('hideleft');
-        defaultClasses();
-        p1Name.value ? Players.player1.name = p1Name.value : Players.player1.name = "Player 1";
-        p2Name.value ? Players.player2.name = p2Name.value : Players.player2.name = "Player 2";
+        if (Players.player1.marker === Players.player2.marker) {
+            alert("error");
+        } else {
+            mainscreen.classList.remove('hideright');
+            mainscreen.classList.add('showleft');
+            settingsscreen.classList.remove('showright');
+            settingsscreen.classList.add('hideleft');
+            defaultClasses();
+            p1Name.value ? Players.player1.name = p1Name.value : Players.player1.name = "Player 1";
+            p2Name.value ? Players.player2.name = p2Name.value : Players.player2.name = "Player 2";
+        }
     });
     btnBackToMain.addEventListener('click', () => {
         mainscreen.classList.remove('hideleft');
